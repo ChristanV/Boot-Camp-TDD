66837756361270d8cfe0725eaac32cefd986dcd7
Christan Vrey <christan.vrey@vcontractor.co.za>
2017-04-05 19:38:37 +0200

    First commit

---
+++ LabXUnit/LabXUnit/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabXUnit
+{
+    public class LeapYearValidator
+    {
+
+        public LeapYearValidator()
+        {
+
+        }
+
+        public bool ValidateYear(int year)
+        {
+            bool isDividedByFour = year % 4 == 0;
+            bool isDividedByHundred = year % 100 == 0;
+            bool isDividedByFourHundred = year % 400 == 0;
+
+            if (isDividedByFour)
+            {
+                if (isDividedByFourHundred && isDividedByHundred)
+                {
+                    return true;
+                }
+                else if (!isDividedByHundred)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ LabXUnit/LabXUnit/TestLeapYear.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LabXUnit
+{
+    public class TestLeapYear
+    {
+
+        [Fact]
+        public void Is_Divisible_By_Four_Test()
+        {
+            var year = 1996;
+            var LeapYear = new LeapYearValidator();
+            bool result = LeapYear.ValidateYear(year);
+
+            Assert.True(result);
+        }
+        [Fact]
+        public void Is_Not_Divisible_By_Four_Test()
+        {
+            var year = 2003;
+            var LeapYear = new LeapYearValidator();
+            bool result = LeapYear.ValidateYear(year);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Should_Be_Divisable_By_4And_100_And_400_Test()
+        {
+            var year = 2000;
+
+            var LeapYear = new LeapYearValidator();
+            bool result = LeapYear.ValidateYear(year);
+
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Should_Be_Divisable_By_4_And_100_But_Not_400_Test()
+        {
+            var year = 1900;
+
+            var LeapYear = new LeapYearValidator();
+            bool result = LeapYear.ValidateYear(year);
+
+            Assert.False(result);
+        }
+
+
+
+    }
+}
